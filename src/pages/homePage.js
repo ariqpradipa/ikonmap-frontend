91722312ad20dc77e0f415efea557e994453c4ce
Ariq Pradipa Santoso <ariq.pradipa@outlook.com>
2022-09-26 08:00:50 +0700

    add axios ans xml fetch

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -3,6 +3,9 @@ import * as React from 'react';
 import Header from '../components/navigation/navbar';
 
 export default function Home() {
+
+  axios
+  .get('https://data.bmkg.go.id/DataMKG/MEWS/DigitalForecast/DigitalForecast-SulawesiSelatan.xml')
   return (
     <>
       <Header />
